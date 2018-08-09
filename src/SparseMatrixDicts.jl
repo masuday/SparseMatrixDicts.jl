46528456b270e953a20bee9ffa0a4a250f65c778
Yutaka Masuda <yutaka@uga.edu>
2018-08-08 23:19:52 -0400

    fix

--- src/SparseMatrixDicts.jl
+++ src/SparseMatrixDicts.jl
@@ -13,7 +13,7 @@ module SparseMatrixDicts
 
 import LinearAlgebra: Symmetric
 import SparseArrays: AbstractSparseArray, SparseMatrixCSC,
-                     nnz, nzrange, sparse
+                     nnz, findnz, nzrange, sparse
 
 import Base: size, show, setindex!, getindex, copy, vec, transpose,
              haskey, fill, fill!, similar
